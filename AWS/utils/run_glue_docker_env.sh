b2b7831d3a9db3c82d04d2860eabaccee7d04393
claudiocmm <claudiofilho22@gmail.com>
2025-03-13 17:03:51 -0300

    adding new project

--- AWS/utils/run_glue_docker_env.sh
+++ AWS/utils/run_glue_docker_env.sh
@@ -2,14 +2,16 @@
 
 
 # Define workspace location and AWS profile (customize as needed)
-WORKSPACE_LOCATION=/home/$USER/Git/data_engineering_projects/AWS  # Adjust this if needed
+WORKSPACE_LOCATION=/home/$USER/Git/data_engineering_projects_temp/AWS  # Adjust this if needed
 PROFILE_NAME=default        # Set your AWS profile name
+DATALAKE_FORMATS=iceberg
 
 
 
 docker run -it -v ~/.aws:/home/glue_user/.aws \
  -v $WORKSPACE_LOCATION:/home/glue_user/workspace/ \
  -e AWS_PROFILE=$PROFILE_NAME \
+ -e DATALAKE_FORMATS=$DATALAKE_FORMATS \
  -e DISABLE_SSL=true \
  --rm -p 4040:4040 \
  -p 18080:18080 \
